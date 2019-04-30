5778efd49f1631daa226642433617c26d95fb746
tzwl <thinzar.winlae@gmail.com>
2019-04-30 16:48:31 +0630

    update login

--- routes/index.js
+++ routes/index.js
@@ -150,11 +150,11 @@ router.get('/login',async(req,res)=>{
       })
       
     }else{
-      // res.send("Fail")
-      res.render("login",{
+      res.send("Fail")
+//       res.render("login",{
 //         inputid: req.query.loginid,
-        errorMessage: "User doesnot exist"
-      })
+//         errorMessage: "User doesnot exist"
+//       })
     }
   })
 })
